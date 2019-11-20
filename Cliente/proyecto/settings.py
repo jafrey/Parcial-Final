461e870a4083449b7799601b64e28079685300fe
Julian Araneda <julianaraneda97@gmail.com>
2019-11-20 14:42:39 -0300

    Modifique ambos settings.py, les instale las aplicaciones que voy a usar, tanto rest_framework como token auth y la app nuestra. la modificacion de estos archivos creo unos archivos de cache que los voy a registrar por que uno nunca sabe

--- Cliente/proyecto/settings.py
+++ Cliente/proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xbzdhd@*ds5u3)^sd%47rq3v@o0&ropmz1^mwcw7zkj#e_lcze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Cosas añadidas a manopla
+    'app',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
