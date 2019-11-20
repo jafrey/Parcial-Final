ad3d8985ca158831cbb6d210adf03fd883ca641d
Julian Araneda <julianaraneda97@gmail.com>
2019-11-20 16:06:40 -0300

    Cree el login, la view, retoque la template y añadi la url, falta desarrollar la template principal y hacer el CRUD

--- Cliente/app/views.py
+++ Cliente/app/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+
+#libreria para que el csrf no rompa los huevos
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+#libreria requests, que nos sirve para consumir apis
+import requests
+
+#libreria json, medio al pedo pero por las dudas x2
+import json
+
+@csrf_exempt
+def login(request):
+
+     if request.method == "GET":
+
+       v = ""
+       return render_to_response('login.html', { 'dato' : v })
+
+     else:
+
+         usuario = request.POST['username']
+         contra = request.POST['password']
+
+
+
+         r = requests.post("http://api:8000/gen-tk/", data={'username': usuario, 'password': contra})
+         jsonData = r.json()
+
+
+         if r.status_code == 400:
+
+             v = 'Algo esta mal en los campos, alguno esta vacío o las credenciales no coinciden con la base de datos.'
+             return render_to_response('login.html', { 'dato' : v })
+
+         elif r.status_code == 200:
+
+             request.session['user'] = usuario
+             request.session['token'] = jsonData["token"]
+             v = 'De culo salió todo bien.'
+             return render_to_response('principal.html', { 'dato' : v })
--- Cliente/proyecto/urls.py
+++ Cliente/proyecto/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#import todas las views de app
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login),
+    path('', views.login),
 ]
