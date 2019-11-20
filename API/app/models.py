32f8b3dd13e59c23203dabd28d636a96d5e91710
Julian Araneda <julianaraneda97@gmail.com>
2019-11-20 14:48:58 -0300

    Cree el modelo cancion y lo registre en admin

--- API/app/models.py
+++ API/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cancion(models.Model):
+    nom_cancion = models.CharField(max_length=30)
+    desc_cancion = models.CharField(max_length=30)
+
+
+    class Meta:
+        verbose_name = "Cancion"
+        verbose_name_plural = "Canciones"
+
+    def __str__(self):
+        return self.nom_cancion
